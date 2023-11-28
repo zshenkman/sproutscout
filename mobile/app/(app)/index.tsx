8926acbc58b0f55c2184d8103c62c4ba41f4b34a
Zachary Shenkman <yankeedude02@gmail.com>
2023-11-28 15:31:53 -0500

    Adds soil and moisture descriptors

--- mobile/app/(app)/index.tsx
+++ mobile/app/(app)/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { supabase } from '../../lib/supabase'
 import { StyleSheet, View, Alert, FlatList, ListRenderItem, ListRenderItemInfo, Image } from 'react-native'
 import { useSession } from '../../contexts/SessionContext'
@@ -62,14 +62,38 @@ export default function Home() {
   }
 
   function renderPlantCard({ item }: ListRenderItemInfo<any>) {
+    const lightDescription = (() => {
+      if (item.light <= 1500) {
+        return "Dark"
+      }
+      else if (item.light <= 3000) {
+        return "Shady"
+      }
+      else if (item.light > 3000) {
+        return "Bright"
+      }
+    })()
+
+    const moistureDescription = (() => {
+      if (item.soil_moisture <= 1500) {
+        return "Wet"
+      }
+      else if (item.soil_moisture <= 3000) {
+        return "Damp"
+      }
+      else if (item.soil_moisture > 3000) {
+        return "Dry"
+      }
+    })()
+
     return (
       <View style={styles.plantCardContainer}>
         <Image source={require('../../assets/images/plant.png')} style={styles.plantCardImage} resizeMode={'contain'}/>
         <Text style={styles.plantCardTitleText}>{item.name}</Text>
         <Text style={styles.plantCardText}>Temperature: <Text style={styles.plantCardValueText}>{Number(item.temperature).toPrecision(3)}° F</Text></Text>
         <Text style={styles.plantCardText}>Humidity: <Text style={styles.plantCardValueText}>{Number(item.humidity).toPrecision(3)}%</Text></Text>
-        <Text style={styles.plantCardText}>Soil Moisture: <Text style={styles.plantCardValueText}>{item.soil_moisture} mL</Text></Text>
-        <Text style={styles.plantCardText}>Light: <Text style={styles.plantCardValueText}>{item.light}</Text></Text>
+        <Text style={styles.plantCardText}>Soil Moisture: <Text style={styles.plantCardValueText}>{moistureDescription} ({item.soil_moisture} mL)</Text></Text>
+        <Text style={styles.plantCardText}>Light: <Text style={styles.plantCardValueText}>{lightDescription}</Text></Text>
       </View>
     )
   }
